f7ba049508f6d2bc89c90f4326cf082ab87c8db3
taoqf <tao_qiufeng@126.com>
2020-03-20 09:48:07 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-export default function fun() {
+import { Linking } from 'react-native';
+
+/**
+ * 打开qq
+ */
+export default function open_qq(): Promise<boolean> {
+	return Linking.openURL('mqq://');
 }
